c7bc146ae07c9845ce692f221568096336d98b2f
Justin Hong <jhong2009@gmail.com>
2018-02-17 16:35:00 -0800

    upload function review part 2

--- Function Review/functionReviewPt2.js
+++ Function Review/functionReviewPt2.js
@@ -3,7 +3,18 @@
 	// multipleLetterCount - this function takes in one parameter (a string) and returns an object
 	// with the keys being the letters and the values being the count of the letter.
 
+	function multipleLetterCount(str) {
+		var letterCount = {};
 
+		for (let i = 0; i < str.length; i++) {
+			if(!(str[i] in letterCount)) {
+				letterCount[str[i]] = 1;
+			} else {
+				letterCount[str[i]]++;
+			}
+		}
+		return letterCount;
+	}
 
 	// test cases
 	multipleLetterCount("hello"); // {h:1, e: 1, l: 2, o:1}
@@ -22,8 +33,21 @@
 		// If the command is "add" and the location is "end", the function should add the value (fourth parameter) to the end of
 		// the array and return the array.
 
-
-
+	function arrayManipulation(arr, cmd, location, val) {
+		if (cmd === 'remove') {
+			if (location === 'end') {
+				return arr.pop();
+			}
+			return arr.shift();
+		} else if (cmd === 'add') {
+			if (location === 'beginning') {
+				arr.unshift(val);
+				return arr;
+			}
+			arr.push(val);
+			return arr;
+		}
+	}	
 
 	// test cases
 	arrayManipulation([1,2,3], "remove", "end"); // 3
@@ -35,10 +59,17 @@
 	// This function should take in one parameter and returns true or false if it is a palindrome.
 	// As a bonus, allow your function to ignore whitespace and capitalization so that isPalindrome('a man a plan a canal Panama'); returns true
 
+	function isPalindrome(str) {
+		// remove whitespace, punctuation, capitalization
+		var str = str.toLowerCase().replace(/[\W_]/g,'');
+		var reverse = str.split('').reverse().join('');
 
+		return str === reverse;
+	}
 
 	// test cases
 	isPalindrome('testing'); // false
 	isPalindrome('tacocat'); // true
 	isPalindrome('hannah'); // true
 	isPalindrome('robert'); // false
+	isPalindrome('a man a plan a canal Panama'); // true
